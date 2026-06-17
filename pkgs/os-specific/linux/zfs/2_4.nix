9eecfb366714bf6193a5e984732e966aae9e722b
Robin Sue <robinsue@live.de>
2026-06-17 02:57:22 +0200

    ZFS: Allow kernel 7.1

--- pkgs/os-specific/linux/zfs/2_4.nix
+++ pkgs/os-specific/linux/zfs/2_4.nix
@@ -3,7 +3,7 @@
   lib,
   nixosTests,
   stdenv,
-  fetchpatch,
+  fetchpatch2,
   ...
 }@args:
 
@@ -13,15 +13,18 @@ callPackage ./generic.nix args {
   kernelModuleAttribute = "zfs_2_4";
 
   kernelMinSupportedMajorMinor = "4.18";
-  kernelMaxSupportedMajorMinor = "7.0";
-
+  kernelMaxSupportedMajorMinor = "7.1";
   # this package should point to the latest release.
   version = "2.4.3";
 
   extraPatches = [
+    (fetchpatch2 {
+      url = "https://github.com/openzfs/zfs/commit/8a9dc3bb70b861332ceb54017ccd7210a69a066c.patch?full_index=1";
+      hash = "sha256-/CEATyuIUejgHFhynvpPmQZZPTz0/6miflXkTTgR1hw=";
+    })
     # https://github.com/openzfs/zfs/issues/18366
     # dedup data corruption fix unreleased as of OpenZFS 2.4.3
-    (fetchpatch {
+    (fetchpatch2 {
       url = "https://github.com/openzfs/zfs/commit/6fb72fda0f60d9efb591e320f83f78b19ec451cc.patch?full_index=1";
       hash = "sha256-UuSVmO61Ux5S3F+JAtRnHyeVS4EFobDTKBuD5s8PI+k=";
     })
